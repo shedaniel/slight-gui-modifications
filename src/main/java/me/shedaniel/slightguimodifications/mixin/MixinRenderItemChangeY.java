52c66c7ebff7720ebe62eed59264df926c46e176
loper12 <finb7729@gmail.com>
2020-11-28 15:15:16 -0600

    Seperate RenderItemChangeY into its own mixin and disable that instead (also actually add the mixin json file)

---
+++ src/main/java/me/shedaniel/slightguimodifications/mixin/MixinRenderItemChangeY.java
@@ -0,0 +1,22 @@
+package me.shedaniel.slightguimodifications.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import me.shedaniel.slightguimodifications.SlightGuiModifications;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemRenderer.class)
+public class MixinRenderItemChangeY {
+    @Inject(method = "render",
+            at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;pushPose()V", shift = At.Shift.AFTER))
+    private void renderItemChangeY(ItemStack stack, ItemTransforms.TransformType renderMode, boolean leftHanded, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo ci) {
+        matrices.translate(0, SlightGuiModifications.applyYAnimation(0) / -16f, 0);
+    }
+}
